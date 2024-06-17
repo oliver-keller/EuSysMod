7e07efcb904f5ff8798889f107d268761ebb45d1
Oliver Keller <oliver-keller@bluewin.ch>
2024-06-17 08:15:23 +0200

    test & run2 are adapted according to run.jl

--- run2.jl
+++ run2.jl
@@ -1,46 +1,82 @@
+
+
+
 using AnyMOD, Gurobi, CSV, Statistics, Dates
 include("support_functions.jl")
 
-t_int = 8 # 4
 
+
+t_int = 8 # Number of threads (4)
+
+# define input and output directories
 inputMod_arr = ["_basis","timeSeries/96hours_2008"]
-resultDir_str = "./results/" * Dates.format(now(), "yyyy-mm-dd_HH-MM-SS")
+# resultDir_str = "./results/151-300/" 
+resultDir_str = "./results/" * Dates.format(now(), "yyyy-mm-dd_HH-MM")
 if !isdir(resultDir_str)
     mkdir(resultDir_str)
 end
-obj_str = "Sampling_1000"
 
+# name of the modelrun
+obj_str = "Sampling_300"
 
 
 # read uncertain parameters
 uncertain_parameters = CSV.read("_basis/uncertain_parameters.csv", DataFrame, types=[String, String, String, String, String, Float64, Float64, Float64, Float64, String])
+# foreach(x -> uncertain_parameters[!,x] = string.(uncertain_parameters[!,x]), [:parameter, :carrier, :region, :technology, :timestep])
 
-lhs = CSV.read("lhs.csv", DataFrame, header=false)
+# lhs = create_lhs(size(uncertain_parameters)[1], 1000, 0) # creates a new Latin Hypercube Sample and saves as csv
+lhs = CSV.read("lhs.csv", DataFrame, header=false) # uses an already created lhc sample
 
-# set iteration counter
-iterations = 200
-outputsOfInterest = nothing
+# set iteration boundaries
+start_iteration = 100
+end_iteration = 200
 
-for iteration in range(100, iterations)
-    println("[info] iteration $iteration/$iterations")
+# initialize model (deepcopy of initialized model might lead to a a terminal crash. In this case initialize the model separately for every iteration.)
+anyM0 = anyModel(inputMod_arr, resultDir_str, objName = obj_str, supTsLvl = 2, repTsLvl = 3, shortExp = 5, emissionLoss = false, holdFixed = true)
 
-    anyM = anyModel(inputMod_arr, resultDir_str, objName = obj_str, supTsLvl = 2, repTsLvl = 3, shortExp = 5, emissionLoss = false, holdFixed = true);
-    # anyM = deepcopy(anyM0)
+outputsOfInterest = nothing # initialize parameter to store the outputs of interest
+objective = DataFrame(iteration = Int[], objectiveValue = Float64[]) # initialize dataframe to store the objective values of the optimization runs
 
-    # modify uncertain parameters
-    for (index, row) in enumerate(eachrow(uncertain_parameters))
-        ismissing(row[:minRel]) || ismissing(row[:maxRel]) ? factor = nothing : factor = row[:minRel]  + (row[:maxRel] - row[:minRel]) * lhs[iteration, index] 
-        ismissing(row[:minAbs]) || ismissing(row[:maxAbs]) ? newValue = nothing : newValue = row[:minAbs] + (row[:maxAbs] - row[:minAbs]) * lhs[iteration, index]
+# set additional constraint for regret calculations -> Set the minimum use of biomass for the following categories (biomass usage in GWh/a)
+set_constraint = true # set to true if additional constraints should be set # make sure that the file "df_input_with_final_cluster.csv" is available in EuSysMod. The file is created by the python script in Decide.
 
-        if row[:parameter] == "trdBuyUp"
-            scaleBiomassPotential(anyM, factor=factor, newValue=newValue, carrier=row[:carrier], region=row[:region])
-        elseif row[:parameter] == "capaConvUp"
-            scaleRenewablePotential(anyM, factor=factor, newValue=newValue, technology=row[:technology], region=row[:region])
-        elseif row[:parameter] == "costExpConv"
-            scaleInvestmentCost(anyM, factor=factor, newValue=newValue, technology=row[:technology])
-        end
+if set_constraint
+    cluster_index = 1
+    new_constraint = Dict(
+        "bioConversionOil" => 0, # 253.5 TWh/a
+        "bioConversionSyngas" =>  94050, # 94 TWh/a
+        "bioConversionBiogas" => 0,
+        "bioConversionChp" => 0,
+        "bioConversionHvc" => 0,
+        "bioConversionCoal" => 0,
+        "networkHeat" => 0,
+        "spaceHeat" => 0,
+        "proHeat" => 0
+    )
+
+    for row in eachrow(anyM0.parts.lim.par[:useLow].data)
+        row.val = get(new_constraint, findTechnology(anyM0, row.Te), row.val)
     end
 
+    #determine if a scenario is already within the respective cluster and should be skipped in the optimization
+    cluster_of_scenarios = CSV.read("df_input_with_final_cluster.csv", DataFrame)[!, "cluster_final"] # make sure that the file "df_input_with_final_cluster.csv" is available in EuSysMod. The file is created by the python script in Decide.
+end
+
+
+for iteration in range(start_iteration, end_iteration)
+    println("[info] iteration $iteration/$end_iteration")
+
+    if set_constraint && cluster_of_scenarios[iteration] == cluster_index
+        continue
+    end
+
+    anyM = deepcopy(anyM0) # deepcopy of initialized model might lead to a a terminal crash => initialize the model for every iteration
+    # anyM = anyModel(inputMod_arr, resultDir_str, objName = obj_str, supTsLvl = 2, repTsLvl = 3, shortExp = 5, emissionLoss = false, holdFixed = true) 
+
+    # modify uncertain parameters
+    modify_parameters(anyM, uncertain_parameters, lhs, iteration)
+
+    # create optimization model
     createOptModel!(anyM)
     setObjective!(:cost, anyM)
 
@@ -49,16 +85,16 @@ for iteration in range(100, iterations)
     set_optimizer_attribute(anyM.optModel, "Method", 2);
     set_optimizer_attribute(anyM.optModel, "Crossover", 0);
     set_optimizer_attribute(anyM.optModel, "Threads",t_int);
-    set_optimizer_attribute(anyM.optModel, "BarConvTol", 1e-3);  # 1e-5
-
+    set_optimizer_attribute(anyM.optModel, "BarConvTol", 1e-5);  # 1e-5
     optimize!(anyM.optModel) # solve the model
-    # plotSankeyDiagram(anyM, dropDown = (:timestep,), name="iteration_" * string(iteration))
-    
-    global outputsOfInterest
-    outputsOfInterest = calculateOutputs(anyM, iteration=iteration, outputsOfInterest=outputsOfInterest, includeWaste=false, resultDir = resultDir_str)
-    
-    
-    # Save outputsOfInterest as a CSV file
-    CSV.write(resultDir_str * "/outputsOfInterest.csv", DataFrame(outputsOfInterest))
-end
 
+    # update the outputs of interest and the objective value dataframes
+    global outputsOfInterest
+    global objective
+    outputsOfInterest = calculateOutputs(anyM, iteration=iteration, outputsOfInterest=outputsOfInterest, includeWaste=false, resultDir = resultDir_str)
+    push!(objective, [iteration, objective_value(anyM.optModel)])
+    
+    # Save outputsOfInterest and total_cost as a CSV file
+    CSV.write(resultDir_str * "/outputsOfInterest.csv", DataFrame(outputsOfInterest))
+    set_constraint ? CSV.write(resultDir_str * "/total_cost_cluster_$cluster_index.csv", DataFrame(objective)) : CSV.write(resultDir_str * "/total_cost.csv", DataFrame(objective))
+end
--- test.jl
+++ test.jl
@@ -1,81 +1,113 @@
 ############### Test Version ####################
 
+
+## Define the input parameter ##
+START_ITERATION = 1               # set iteration boundaries
+END_ITERATION = 1                 # set iteration boundaries
+OBJ_STR_INPUT = "exvhange_test"                # name of the modelrun
+MIN_BIOMASS_FOR_OIL = 0         # constraint for using certain amount of biomass for oil produiction (TWh/a)
+MIN_BIOMASS_FOR_HVC = 0         # constraint for using certain amount of biomass for HVC produiction (TWh/a)
+MIN_BIOMASS_FOR_SYNGAS = 0      # constraint for using certain amount of biomass for syngas produiction (TWh/a)
+CLUSTER_INDEX = 0               # cluster index for the additional constraint (if no constraint parameter is not used) 
+BARRIER_CONV_TOL = 1e-3         # barrier convergence tolerance
+NUMERIC_FOCUS = 0               # numeric focus for the solver
+
+
 using AnyMOD, Gurobi, CSV, Statistics, Dates
 include("support_functions.jl")
 
-t_int = 8 # Number of threads (4)
+t_int = 8 # number of threads
 
-inputMod_arr = ["_basis","timeSeries/96hours_2008"]
-resultDir_str = "./results/" * Dates.format(now(), "yyyy-mm-dd_HH-MM")
-if !isdir(resultDir_str)
-    mkdir(resultDir_str)
-end
+# define the name of the modelrun
+OBJ_STR = OBJ_STR_INPUT * "_iteraiton" * string(START_ITERATION) * "-" * string(END_ITERATION) * "_oil" * string(MIN_BIOMASS_FOR_OIL) * "_hvc" * string(MIN_BIOMASS_FOR_HVC) * "_syngas" * string(MIN_BIOMASS_FOR_SYNGAS) * "_cluster" * string(CLUSTER_INDEX)
 
-obj_str = "test"
-# obj_str = "Test_" * Dates.format(now(), "yyyy-mm-dd_HH-MM")
+# define input and output directories
+inputMod_arr = ["./_basis","./timeSeries/96hours_2008"]
+resultDir_str = "./results/" * OBJ_STR * Dates.format(now(), "_yyyy-mm-dd_HH-MM")
+mkdir(resultDir_str)
+
 
 # read uncertain parameters
 uncertain_parameters = CSV.read("_basis/uncertain_parameters.csv", DataFrame, types=[String, String, String, String, String, Float64, Float64, Float64, Float64, String])
-# foreach(x -> uncertain_parameters[!,x] = string.(uncertain_parameters[!,x]), [:parameter, :carrier, :region, :technology, :timestep])
 
-# lhs = create_lhs(size(uncertain_parameters)[1], 1000, 0)
-lhs = CSV.read("lhs.csv", DataFrame, header=false)
+# lhs = create_lhs(size(uncertain_parameters)[1], 1000, 0) # creates a new Latin Hypercube Sample and saves as csv
+lhs = CSV.read("lhs.csv", DataFrame, header=false) # uses an already created lhc sample
 
-anyM = anyModel(inputMod_arr, resultDir_str, objName = obj_str, supTsLvl = 2, repTsLvl = 3, shortExp = 5, emissionLoss = false, holdFixed = true)
 
-iteration = 206
+outputsOfInterest = nothing # initialize parameter to store the outputs of interest
+objective = DataFrame(iteration = Int[], objectiveValue = Float64[]) # initialize dataframe to store the objective values of the optimization runs
 
-# modify uncertain parameters
-modify_parameters(anyM, uncertain_parameters, lhs, iteration)
+# set additional constraint for regret calculations -> Set the minimum use of biomass for the following categories (biomass usage in GWh/a)
+# make sure that the file "df_input_with_final_cluster.csv" is available in EuSysMod. The file is created by the python script in Decide.
+set_constraint = (MIN_BIOMASS_FOR_HVC > 0) || (MIN_BIOMASS_FOR_OIL > 0) || (MIN_BIOMASS_FOR_SYNGAS > 0) # set to true if additional constraints exists
 
-# set additional constraint for regret calculations -> Set the minimum use of biomass for the following categories
-new_constraint = Dict(
-    "bioConversionOil" => 235500,
-    "bioConversionSyngas" => 0,
-    "bioConversionBiogas" => 0,
-    "bioConversionChp" => 0,
-    "bioConversionHvc" => 0,
-    "bioConversionCoal" => 0,
-    "networkHeat" => 0,
-    "spaceHeat" => 0,
-    "proHeat" => 0
-)
+if set_constraint
+    new_constraint = Dict(
+        "bioConversionOil" => MIN_BIOMASS_FOR_OIL*1000,
+        "bioConversionSyngas" => MIN_BIOMASS_FOR_SYNGAS*1000, 
+        "bioConversionBiogas" => 0, 
+        "bioConversionChp" => 0,
+        "bioConversionHvc" => MIN_BIOMASS_FOR_HVC*1000,
+        "bioConversionCoal" => 0,
+        "networkHeat" => 0,
+        "spaceHeat" => 0,
+        "proHeat" => 0
+    )
 
-for row in eachrow(anyM.parts.lim.par[:useLow].data)
-    row.val = get(new_constraint, findTechnology(anyM, row.Te), row.val)
+    for row in eachrow(anyM0.parts.lim.par[:useLow].data)
+        row.val = get(new_constraint, findTechnology(anyM0, row.Te), row.val)
+    end
+
+    #determine if a scenario is already within the respective cluster and should be skipped in the optimization
+    cluster_of_scenarios = CSV.read("df_filtered_with_cluster.csv", DataFrame)[!, "cluster_final"] # make sure that the file "df_filtered_with_cluster.csv" is available in EuSysMod. The file is created by the python script in Decide.
 end
 
-anyM.parts.lim.par[:useLow].data
 
+anyM = anyModel(inputMod_arr, resultDir_str, objName = OBJ_STR, supTsLvl = 2, repTsLvl = 3, shortExp = 5, emissionLoss = false, holdFixed = true) 
 
-# solve model
+# modify uncertain parameters
+iteration = 1
+modify_parameters(anyM, uncertain_parameters, lhs, iteration)
+
+# set exchange costs to zero to see if biomass is exchanged between regions
+for rows in eachrow(anyM.parts.cost.par[:costVarExc].data)
+    rows.val = 0
+end
+
+# create optimization model
 createOptModel!(anyM)
 setObjective!(:cost, anyM)
-set_optimizer(anyM.optModel, Gurobi.Optimizer) # select a solver
+
+# solve model
+set_optimizer(anyM.optModel, Gurobi.Optimizer)  # select a solver
 set_optimizer_attribute(anyM.optModel, "Method", 2);
 set_optimizer_attribute(anyM.optModel, "Crossover", 0);
 set_optimizer_attribute(anyM.optModel, "Threads",t_int);
-set_optimizer_attribute(anyM.optModel, "BarConvTol", 1e-5);  # 1e-5
+set_optimizer_attribute(anyM.optModel, "BarConvTol", BARRIER_CONV_TOL); 
+set_optimizer_attribute(anyM.optModel, "NumericFocus", NUMERIC_FOCUS); 
 optimize!(anyM.optModel) # solve the model
+
 
 
 ##### get output of the model #####
 
 
+# update the outputs of interest and the objective value dataframes
+outputsOfInterest = calculateOutputs(anyM, iteration=iteration, outputsOfInterest=outputsOfInterest, includeWaste=false, resultDir = resultDir_str)
+push!(objective, [iteration, objective_value(anyM.optModel)])
+
+# Save outputsOfInterest and total_cost as a CSV file
+CSV.write(resultDir_str * "/outputsOfInterest.csv", DataFrame(outputsOfInterest))
+set_constraint ? CSV.write(resultDir_str * "/total_cost_cluster_$CLUSTER_INDEX.csv", DataFrame(objective)) : CSV.write(resultDir_str * "/total_cost.csv", DataFrame(objective))
+
+
 total_cost = objective_value(anyM.optModel) # objective value (in mio Euro)
 
 
-outputsOfInterest = nothing
-outputsOfInterest = calculateOutputs(anyM, iteration=iteration, outputsOfInterest=outputsOfInterest, includeWaste=false)
-
-
-
-
-
 reportResults(:summary,anyM, addRep = (:capaConvOut,), addObjName = true)
-# reportResults(:exchange,anyM, addObjName = true)
-# reportResults(:cost,anyM, addObjName = true)
-# reportTimeSeries(:electricity,anyM)
+reportResults(:exchange,anyM, addObjName = true)
+reportResults(:cost,anyM, addObjName = true)
+reportTimeSeries(:electricity,anyM)
 
 
 plotSankeyDiagram(anyM, dropDown = (:timestep,)) # sankey for the whole europe
